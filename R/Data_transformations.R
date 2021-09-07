ceb7d2476aa38a51de8138765b7a090a454401a7
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2021-09-07 21:19:56 +0200

    update

--- R/Data_transformations.R
+++ R/Data_transformations.R
@@ -2,7 +2,7 @@ Sys.setenv(RGL_USE_NULL="TRUE")
 
 #' @import dplyr
 #' @import colorspace
-#' @importFrom Bioconductor widgetTools
+#' @importFrom BiocManager widgetTools
 #' @importFrom geometry convhulln
 #' @importFrom pracma distmat
 #' @importFrom ptinpoly pip3d
