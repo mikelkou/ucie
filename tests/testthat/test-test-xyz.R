8013ae01a9df38942a5b83c53e77b12b163ce0a9
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2021-09-08 18:21:46 +0200

    Update test-test-xyz.R

--- tests/testthat/test-test-xyz.R
+++ tests/testthat/test-test-xyz.R
@@ -8,17 +8,7 @@ test_that("data2cielab works", {
 
   data_with_colors <- data2cielab(df, Wb=1.2, S=1.6)
   suppressWarnings(
-  expect_that( data_with_colors, is_a("data.frame") ) )
-  suppressWarnings(
   expect_that( ncol(data_with_colors), equals(2) )
   )
 })
 
-# test_that("data2cielab with LAB coords works", {
-#
-#   data_with_colors_with_LAB <- data2cielab(df, Wb=1.2, S=1.6, LAB_coordinates = T)
-#   suppressWarnings(
-#   expect_that( data_with_colors_with_LAB, is_a("data.frame") ) )
-#   suppressWarnings(
-#   expect_that( ncol(data_with_colors_with_LAB), equals(4) ) )
-# })
