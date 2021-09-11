960ae87574b78f888796937c55c9c28aed8d0985
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2021-09-11 20:40:56 +0200

    Update test-test-xyz.R

--- tests/testthat/test-test-xyz.R
+++ tests/testthat/test-test-xyz.R
@@ -1,8 +1,8 @@
-df <- data.frame(V1=runif(2,  0,1), V2=runif(2,  0,5))
+df <- data.frame(V1=runif(2,  0,1), V2=runif(2,  0,5), V3=runif(2,  0,30), V4=runif(2,  0,30))
 
 test_that("data2cielab works", {
   suppressWarnings(
-    expect_that(data2cielab(df), gives_warning())
+    expect_error(data2cielab(df))
   )
 })
 
